7d2ade5e4e1cfb0d31b2d7fe795b81b719bf978d
fateme adiban <f.adiban.rad@gmail.com>
2025-09-17 22:31:09 +0330

    initial commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,114 @@
+"use client"
+import { useEffect } from "react"
+import gsap from "gsap"
+import styles from "./style.module.css"
+import Image from "next/image"
+
+export default function Index() {
+  useEffect(() => {
+    gsap.set(`.${styles.header}`, { y: -200 })
+    gsap.to(`.${styles.header}`, {
+      y: 0,
+      duration: 1,
+      ease: "circ.out",
+      delay: 3
+    })
+  }, [])
+
+  return (
+    <>
+      <div className={styles.header}>
+        <div className={styles.nav}>
+          <div className={styles.el}>
+            <a
+              href="#about"
+              onClick={e => {
+                e.preventDefault()
+                const about = document.querySelector("#about")
+                if (about) {
+                  const y = about.getBoundingClientRect().top + window.pageYOffset
+                  window.scrollTo({ top: y, behavior: "smooth" })
+                }
+              }}
+            >
+              About
+            </a>
+          </div>
+
+          <div className={styles.el}>
+            <a
+              href="#gallery"
+              onClick={e => {
+                e.preventDefault()
+                const gallery = document.querySelector("#gallery")
+                if (gallery) {
+                  const y = gallery.getBoundingClientRect().top + window.pageYOffset
+                  window.scrollTo({ top: y, behavior: "smooth" })
+                }
+              }}
+            >
+              Gallery
+            </a>
+          </div>
+
+          <div className={styles.el}>
+            <a
+              href="#services"
+              onClick={e => {
+                e.preventDefault()
+                const services = document.querySelector("#services")
+                if (services) {
+                  const y = services.getBoundingClientRect().top + window.pageYOffset
+                  window.scrollTo({ top: y, behavior: "smooth" })
+                }
+              }}
+            >
+              Services
+            </a>
+          </div>
+
+          <div className={styles.el}>
+            <a
+              href="#testimonials"
+              onClick={e => {
+                e.preventDefault()
+                const testimonials = document.querySelector("#testimonials")
+                if (testimonials) {
+                  const y = testimonials.getBoundingClientRect().top + window.pageYOffset
+                  window.scrollTo({ top: y, behavior: "smooth" })
+                }
+              }}
+            >
+              Testimonials
+            </a>
+          </div>
+
+          <div className={styles.el}>
+            <a
+              href="#contact"
+              onClick={e => {
+                e.preventDefault()
+                const contact = document.querySelector("#contact")
+                if (contact) {
+                  const y = contact.getBoundingClientRect().top + window.pageYOffset
+                  window.scrollTo({ top: y, behavior: "smooth" })
+                }
+              }}
+            >
+              Contact
+            </a>
+          </div>
+        </div>
+
+        <div className={styles.logo}>
+          <p>Lelde Legzdina</p>
+        </div>
+
+        <div className={styles.icons}>
+          <Image onClick={() => window.open("https://www.linkedin.com/in/leldelegzdina", "_blank")} src="/linkedin.svg" width={16} height={16} alt="linkedin icon" />
+          <Image onClick={() => window.open("mailto:legzdinalelde@gmail.com")} src="/email.svg" width={20} height={16} alt="email icon" />
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,34 @@
+"use client"
+import React from "react"
+import Image from "next/image"
+
+export default function Index() {
+  return (
+    <section id="home" className="relative min-h-screen overflow-x-hidden flex flex-col md:flex-row gap-[8%]">
+      <div className="relative w-full sm:w-auto">
+        <Image src="/img-hero.png" width={720} height={917} alt="hero image" className="w-full h-screen md:max-w-[720px] md:h-[917px]" />
+
+        <div className="absolute inset-0 flex flex-col justify-end mb-10 items-center md:hidden bg-black/40">
+          <h2 className="text-[18px] font-noto-serif uppercase mb-4 text-white">Personal & corporate branding</h2>
+          <h1 className="text-[30px] leading-[110%] font-instrument-serif text-center max-w-[65%] sm:max-w-[45%] text-white">Build LinkedIn clarity, confidence & community in 60 days</h1>
+        </div>
+      </div>
+
+      <div className="hidden md:flex flex-col justify-center items-center pr-10 2xl:pr-0">
+        <h2 className="text-[18px] lg:text-[20px] font-noto-serif uppercase mb-4">Personal & corporate branding</h2>
+        <h1 className="text-[30px] lg:text-[50px] xl:text-[60px] leading-[110%] font-instrument-serif text-center max-w-[500px] xl:max-w-[550px]">Build LinkedIn clarity, confidence & community in 60 days</h1>
+        <div className="relative flex items-center mt-15">
+          <button onClick={() => window.open("https://calendly.com/leldel/30minchat", "_blank")} className="text-[18px] font-noto-serif uppercase cursor-pointer">
+            Book a call
+          </button>
+          <Image src="/red-arrow.svg" width={117} height={47} alt="red arrow icon" className="absolute -left-15 top-18 -translate-y-1/2" />
+        </div>
+      </div>
+
+      <div className="absolute bottom-20 right-2 hidden md:flex flex-col items-center gap-7">
+        <p className="text-[16px] font-cutive-mono transform rotate-[-270deg]">SCROLL</p>
+        <Image src="/arrow.svg" width={12} height={16} alt="arrow icon" />
+      </div>
+    </section>
+  )
+}
